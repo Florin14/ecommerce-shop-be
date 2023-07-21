86da4159c6eb58b92cacf38f599d06463b909c3b
Zimbru Florin <zimbru.florin.4@gmail.com>
2023-07-22 00:37:05 +0300

    Initial commit

---
+++ src/test/java/com/example/ecommerceshopbe/EcommerceShopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerceshopbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceShopBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
